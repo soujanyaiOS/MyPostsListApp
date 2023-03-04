2a2860c2fbb312f77e4c4576190a38a5b1a0f450
soujanyaiOS <nsoujanya.ios@gmail.com>
2023-03-04 11:58:50 +0100

    Initial Commit

---
+++ MyPostsApp/MyPostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPostsApp
+//
+//  Created by soujanya Balusu on 04/03/23.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
